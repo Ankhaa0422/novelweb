27042f043169ee4ec0e2dadda51816ff6ddfe0ec
Ankhaa <ankhaa.akire@gmail.com>
2024-05-17 18:00:11 +0800

    zs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useEffect, useLayoutEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+
+    useLayoutEffect(() => {
+        async function getData() {
+            const res = await fetch('https://qs3ek1it0j.execute-api.ap-southeast-1.amazonaws.com/api/list')
+            const data = await res.json()
+            console.log("data ==>", data)
+        }
+        getData()
+    }, [])
+
+    useEffect(() => {
+        
+    }, [])
+
     return (
         <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`} >
         </main>
